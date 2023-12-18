819b00e5750ef4d3432bd0185a168d8102547e2f
devMeno <charlesamenlokonon@gmail.com>
2023-12-18 23:44:34 +0100

    A few modification before design starting

--- src/App.jsx
+++ src/App.jsx
@@ -98,17 +98,7 @@ function App() {
                                         <div className="text-center">
 
                                             <div className="w-full">
-                                                {/* <Tabs aria-label="Pills" style="pills" className='' color='primary'>
-        <Tabs.Item active title="Tab 1" className='text-blue-500'>
-            <p className="text-sm text-blue-500 dark:text-gray-400">Content 1</p>
-        </Tabs.Item>
-        <Tabs.Item title="Tab 2">
-            <p className="text-sm text-gray-500 dark:text-gray-400">Content 2</p>
-        </Tabs.Item>
-        <Tabs.Item title="Tab 3">
-            <p className="text-sm text-gray-500 dark:text-gray-400">Content 3</p>
-        </Tabs.Item>
-    </Tabs> */}
+
                                                 <span className='font-medium mt-3'>New transaction</span>
                                                 <h1 className='text-6xl font-bold my-4'>{amount}<span className='text-4xl'>€</span> </h1>
                                                 <input type="date" id="" aria-describedby="helper-text-explanation" className="bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-gray-700 focus:border-gray-700 block w-full p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 my-3" onChange={handleDateChange} />
